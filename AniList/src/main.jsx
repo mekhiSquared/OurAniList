18f8efe90f925c5aa4d8b386d90159210878f622
Mekhi-M <mekhim0731@gmail.com>
2024-08-21 16:25:24 -0400

    ReactRouterDom i && BrowserRouter component wrapped around app

--- AniList/src/main.jsx
+++ AniList/src/main.jsx
@@ -5,11 +5,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AnimeProvider from "./context/AnimeProvider.jsx";
+import BrowserRouter from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<AnimeProvider>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
 	</AnimeProvider>
 );
