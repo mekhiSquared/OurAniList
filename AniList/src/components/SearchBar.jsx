506c9c853cf2cd412e3ecb8ec31838aa7e590fef
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-08-22 14:09:59 -0400

    search bar

--- AniList/src/components/SearchBar.jsx
+++ AniList/src/components/SearchBar.jsx
@@ -1,1 +1,34 @@
+import { handleFetch } from "../utils";
+import { useContext, useState } from "react";
+import AnimeContext from "../context/AnimeContext";
 
+export const SearchBar = () => {
+  const { setFetchError } = useContext(AnimeContext);
+  const [search, setSearch] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    let [data, error] = await handleFetch(
+      ` https://api.jikan.moe/v4/anime?q=${search}&sfw`
+    );
+
+    console.log(data.data);
+    if (error) setFetchError(error);
+  };
+
+  console.log(search);
+
+  return (
+    <form onSubmit={submit}>
+      <label htmlFor="searchBar"></label>
+      <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit">SEARCH</button>
+    </form>
+  );
+};
