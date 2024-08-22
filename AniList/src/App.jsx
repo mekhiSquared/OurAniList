fc37f9492c634c08a05727c55948c822321ceb78
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-08-22 13:39:56 -0400

    added components folder

--- AniList/src/App.jsx
+++ AniList/src/App.jsx
@@ -4,13 +4,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import AnimeContext from "./context/AnimeContext";
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
-	const { fetchError } = useContext(AnimeContext);
+  const { fetchError } = useContext(AnimeContext);
 
-	if (fetchError) return <p>{errorState.message}</p>;
+  if (fetchError) return <p>{errorState.message}</p>;
 
-	return;
+  return <SearchBar />;
 }
 
 export default App;
