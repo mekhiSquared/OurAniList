119ac4f21a3cd0cfb7ac0a9d8ce2bf86c2a250cd
Mekhi-M <mekhim0731@gmail.com>
2024-08-22 17:54:12 -0400

    query submission navs to /result path + search bar added to nav bar

--- AniList/src/components/NavBar.jsx
+++ AniList/src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 /** @format */
 
+import { SearchBar } from "./SearchBar";
 import ToHomeButton from "./ToHomeButton";
 import ToTrendingButton from "./ToTrendingButton";
 
 const NavBar = () => {
 	return (
-		<>
+		<nav>
 			<ToHomeButton />
 			<ToTrendingButton />
-		</>
+			<SearchBar />
+		</nav>
 	);
 };
 
--- AniList/src/pages/NotFoundPage.jsx
+++ AniList/src/pages/NotFoundPage.jsx
@@ -1,9 +1,11 @@
 /** @format */
+import NavBar from "../components/NavBar";
 import ToHomeButton from "../components/ToHomeButton";
 
 const NotFoundPage = () => {
 	return (
 		<>
+			<NavBar />
 			<h1>Page not Found</h1>
 			<ToHomeButton />
 		</>
