88ceebf8847e9cd24c389d8ef0a26a3a40467025
Mekhi-M <mekhim0731@gmail.com>
2024-08-23 14:06:36 -0400

    added last Nav pathway + a lil more :) (my b)

--- AniList/src/components/CardComponent.jsx
+++ AniList/src/components/CardComponent.jsx
@@ -1,10 +1,23 @@
+/** @format */
+
+import { useNavigate } from "react-router-dom";
+
 export const CardComponent = ({ anime }) => {
-  return (
-    <>
-      <figure id="fig">
-        <img id="cardImg" src={anime.images.jpg.image_url} alt="anime-photo" />
-        <figcaption>{anime.titles[0].title}</figcaption>
-      </figure>
-    </>
-  );
+	const navigate = useNavigate();
+	return (
+		<>
+			<figure
+				id='fig'
+				onClick={() => navigate(`/anime/${anime.mal_id}`)}>
+				<img
+					id='cardImg'
+					src={anime.images?.jpg.image_url}
+					alt='anime-photo'
+				/>
+				<figcaption>
+					{anime.titles ? anime.titles[0].title : undefined}
+				</figcaption>
+			</figure>
+		</>
+	);
 };
