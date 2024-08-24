26306a9db0462c2eb19bd6ed4743d122eef1fdae
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-08-24 02:03:32 -0400

    comments added

--- AniList/src/pages/AnimeSpecsPage.jsx
+++ AniList/src/pages/AnimeSpecsPage.jsx
@@ -11,30 +11,30 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const AnimeSpecsPage = () => {
-	const [anime, setAnime] = useState({});
-	const { id } = useParams();
-	const navigate = useNavigate();
-	const { dependencyFlag } = useContext(AnimeContext);
+  const [anime, setAnime] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { dependencyFlag } = useContext(AnimeContext);
 
-	useEffect(() => {
-		const doFetch = async () => {
-			const [data, error] = await handleFetch(
-				`https://api.jikan.moe/v4/anime/${id}/full`
-			);
-			if (data) setAnime(data.data);
-			if (error) navigate(`*`);
-		};
-		doFetch();
-	}, [dependencyFlag]);
+  useEffect(() => {
+    const doFetch = async () => {
+      const [data, error] = await handleFetch(
+        `https://api.jikan.moe/v4/anime/${id}/full`
+      );
+      if (data) setAnime(data.data);
+      if (error) navigate(`*`);
+    };
+    doFetch();
+  }, [dependencyFlag]);
 
-	return (
-		<>
-			<NavBar />
-			<p>Anime Specs {`id: ${anime.mal_id}`}</p>
-			<CardComponent anime={anime} />
-			<TopTenComponent />
-		</>
-	);
+  return (
+    <>
+      <NavBar />
+      <p>Anime Specs {`id: ${anime.mal_id}`}</p>
+      <h1>{anime.titles[0].title}</h1>
+      {/* <TopTenComponent /> commented out for now*/}
+    </>
+  );
 };
 
 export default AnimeSpecsPage;
